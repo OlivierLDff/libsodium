2293085dad868fb85bcd654c6c190a54043a6eb9
OlivierLdff <olivier.ldff@gmail.com>
2019-08-20 17:48:38 +0200

    Fix ATOMIC OPS test

--- test/HAVE_ATOMIC_OPS.c
+++ test/HAVE_ATOMIC_OPS.c
@@ -2,5 +2,6 @@ int main()
 {
     static volatile int _sodium_lock;
     __sync_lock_test_and_set(&_sodium_lock, 1);
-    __sync_lock_release(&_sodium_lock);}
+    __sync_lock_release(&_sodium_lock);
     return 0;
+}
